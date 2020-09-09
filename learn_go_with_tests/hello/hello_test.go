cd397f451baf4ffab6a06a9bbe660c5093f1994a
phiratio <phiratio161@gmail.com>
2020-09-09 09:26:00 +0300

    Hello world

---
+++ learn_go_with_tests/hello/hello_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+    assertCorrectMessage := func(t *testing.T, got, want string) {
+		// t.Helper() is needed to tell the test suite that this method is a helper.
+		// By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
+		// This will help other developers track down problems easier.
+        t.Helper()
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    }
+
+    t.Run("saying hello to people", func(t *testing.T) {
+        got := Hello("bob")
+        want := "Hello, bob!"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("empty string defaults to 'World'", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello, World!"
+        assertCorrectMessage(t, got, want)
+    })
+
+}
