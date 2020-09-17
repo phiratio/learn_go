adb32a798a9c056953a3b643c4caca409b37ae95
phiratio <phiratio161@gmail.com>
2020-09-17 12:39:55 +0300

    arrays module finished

--- learn_go_with_tests/arrays_slices/sum.go
+++ learn_go_with_tests/arrays_slices/sum.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Sum(numbers []int) int {
 	sum := 0
@@ -10,7 +12,30 @@ func Sum(numbers []int) int {
 	return sum
 }
 
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int //nolint:prealloc
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
+
 func main() {
-	var op = Sum([]int{1, 2, 3, 4, 5})
+	var op = SumAll([]int{1, 2}, []int{3, 4})
 	fmt.Println(op)
 }
